f1dfc2e144b3f04d75725037caaa7cad8d8ee475
pandravrc <162582491+pandravrc@users.noreply.github.com>
2024-09-29 13:41:24 +0900

    Initial commit

---
+++ Runtime/FlatPlus1Patch.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using nadena.dev.ndmf;
+
+namespace com.github.pandrabox.flatplus1patch
+{
+    [AddComponentMenu("Pan/FlatPlus1Patch")]
+    public class FlatPlus1Patch : MonoBehaviour 
+    { 
+    }
+}
